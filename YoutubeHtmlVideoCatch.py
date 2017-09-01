9d561171313bf582196a9e8b956eafc21b3ab533
Vbon <5914018@qq.com>
2017-09-01 17:30:48 +0800

    update

--- YoutubeHtmlVideoCatch.py
+++ YoutubeHtmlVideoCatch.py
@@ -6,7 +6,10 @@
 
 from bs4 import BeautifulSoup
 import urllib.request
-
+import os
+import ssl
+# 设置 全局取消证书验证 http://blog.csdn.net/moonhillcity/article/details/52767999
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def downloadpage(url):
     fp = urllib.request.urlopen(url)
@@ -16,16 +19,28 @@ def downloadpage(url):
 
 
 def parsehtml(data):
-    soup = BeautifulSoup(''.join(data))
-    for x in soup.findAll('a'):
-        print
-        x.attrs['href']
+    # soup = BeautifulSoup(data,'lxml')
+    # url_watch_list = []
+    # for x in soup.findAll('a'):
+    #     a_href = x.attrs['href']
+    #     if 'https://www.youtube.com/watch' in a_href:
+    #         url_watch_list.append(a_href)
+    #     else:
+    #         if '/watch' in a_href:
+    #             url_watch_list.append('https://www.youtube.com' + a_href)
+    #
+    #
+    # print(url_watch_list)
+
+    os.system('cd /Users/Vbon/Desktop/YoutubeDL')
+    print(os.system('youtube-dl https://www.youtube.com/watch\?v\=Zbdjk_Bv4yg'))
+
 
 
 if __name__ == "__main__":
-    parsehtml(downloadpage('http://www.baidu.com'))
+    parsehtml(downloadpage('https://www.youtube.com/watch?v=Zbdjk_Bv4yg'))
     # parsehtml("""
     # <a href="www.google.com"> google.com</a>
     # <A Href="www.pythonclub.org"> PythonClub </a>
     # <A HREF = "www.sina.com.cn"> Sina </a>
-    # """)
+    # """)x.attrs['href']
