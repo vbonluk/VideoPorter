b0030bdd4b383bd0a63ec6093fb6b027ef8b8d8a
Vbon <5914018@qq.com>
2017-09-01 18:02:25 +0800

    update

---
+++ You_dl_exc.sh
@@ -0,0 +1,3 @@
+cd /Users/Vbon/Desktop/YoutubeDL
+
+youtube-dl https://www.youtube.com/watch\?v\=Zbdjk_Bv4yg
--- YoutubeHtmlVideoCatch.py
+++ YoutubeHtmlVideoCatch.py
@@ -7,6 +7,7 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import os
+import sys
 import ssl
 # 设置 全局取消证书验证 http://blog.csdn.net/moonhillcity/article/details/52767999
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -31,11 +32,20 @@ def parsehtml(data):
     #
     #
     # print(url_watch_list)
+    ss_file_path = cur_file_dir() + '/You_dl_exc.sh'
+    exc = 'bash ' + ss_file_path
+    print(os.system(exc))
 
-    os.system('cd /Users/Vbon/Desktop/YoutubeDL')
-    print(os.system('youtube-dl https://www.youtube.com/watch\?v\=Zbdjk_Bv4yg'))
 
-
+#获取脚本文件的当前路径
+def cur_file_dir():
+     #获取脚本路径
+     path = sys.path[0]
+     #判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
+     if os.path.isdir(path):
+         return path
+     elif os.path.isfile(path):
+         return os.path.dirname(path)
 
 if __name__ == "__main__":
     parsehtml(downloadpage('https://www.youtube.com/watch?v=Zbdjk_Bv4yg'))
